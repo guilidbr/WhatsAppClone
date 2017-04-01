a1abdf563ccd019600d906a05c36504d44531179
Guilherme Dambrós <Guilherme Dambrs>
2017-03-31 23:50:45 -0300

    first commit

---
+++ app/src/main/java/android/guilhermedambros/whatsappclone/activity/ValidadorActivity.java
@@ -0,0 +1,14 @@
+package android.guilhermedambros.whatsappclone.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.guilhermedambros.whatsappclone.R;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
