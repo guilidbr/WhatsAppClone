7ba3b3ecc2c19dd6f94dbef1590cf24efde2e93a
Guilherme Dambrós <Guilherme Dambrs>
2017-04-03 18:35:08 -0300

    Login de usuário

--- app/src/main/java/android/guilhermedambros/whatsappclone/activity/LoginActivity.java
+++ app/src/main/java/android/guilhermedambros/whatsappclone/activity/LoginActivity.java
@@ -4,10 +4,12 @@ import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.guilhermedambros.whatsappclone.Model.Usuario;
 import android.guilhermedambros.whatsappclone.config.ConfiguracaoFirebase;
 import android.guilhermedambros.whatsappclone.helper.Permissao;
 import android.guilhermedambros.whatsappclone.helper.Preferencias;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +24,12 @@ import android.widget.Toast;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.github.rtoshiro.util.format.text.SimpleMaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
@@ -30,15 +38,79 @@ import java.util.Random;
 public class LoginActivity extends AppCompatActivity {
 
     private DatabaseReference referenciaFirebase;
+    private EditText email;
+    private EditText senha;
+    private Button logar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        referenciaFirebase = ConfiguracaoFirebase.getFirebase();
-        referenciaFirebase.child("pontos").setValue("800");
+        email = (EditText) findViewById(R.id.edit_login_email);
+        senha = (EditText) findViewById(R.id.edit_login_senha);
+        logar = (Button) findViewById(R.id.btn_logar);
 
+        verificarUsuarioLogado();
+        logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((email.getText().toString().equals("")) || (senha.getText().toString().equals(""))){
+                    Toast.makeText(LoginActivity.this, "Preencha todos os campos!", Toast.LENGTH_LONG).show();
+                }else {
+                    usuario = new Usuario();
+                    usuario.setEmail(email.getText().toString());
+                    usuario.setSenha(senha.getText().toString());
+                    validarLogin();
+                }
+            }
+        });
+
+    }
+
+    private void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if(autenticacao.getCurrentUser() != null){
+            abrirTelaPrincipal();
+        }
+    }
+
+    private void validarLogin(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Sucesso ao logar.", Toast.LENGTH_LONG).show();
+
+                }else{
+                    String erro = "";
+                    try{
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidCredentialsException e ){
+                        erro = "E-mail ou senha incorretos. Tente novamente!";
+                    }catch (FirebaseAuthInvalidUserException e ){
+                        erro = "E-mail ou senha incorretos. Tente novamente!";
+                    }catch (Exception e){
+                        erro = "Erro ao logar. Tente novamente!";
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, "Erro: "+erro, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void abrirCadastroUsuario(View view) {
--- app/src/main/java/android/guilhermedambros/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/android/guilhermedambros/whatsappclone/activity/MainActivity.java
@@ -1,20 +1,39 @@
 package android.guilhermedambros.whatsappclone.activity;
 
+import android.content.Intent;
 import android.guilhermedambros.whatsappclone.R;
 import android.guilhermedambros.whatsappclone.config.ConfiguracaoFirebase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button btnSair;
+    private FirebaseAuth autenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSair = (Button) findViewById(R.id.btn_sair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 }
