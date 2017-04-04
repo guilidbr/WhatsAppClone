3a52b4253eaeadf11219313fe4cee3c2f898b876
Guilherme Dambrós <Guilherme Dambrs>
2017-04-03 22:59:02 -0300

    Sliding Tabs - parte I

---
+++ app/src/main/java/android/guilhermedambros/whatsappclone/fragment/ContatosFragment.java
@@ -0,0 +1,30 @@
+package android.guilhermedambros.whatsappclone.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.guilhermedambros.whatsappclone.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContatosFragment extends Fragment {
+
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contatos, container, false);
+    }
+
+}
