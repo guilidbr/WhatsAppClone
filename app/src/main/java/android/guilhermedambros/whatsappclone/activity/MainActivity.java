993d659e52c55a5b4722b736fca2e18314242a44
Guilherme Dambrós <Guilherme Dambrs>
2017-04-06 18:39:07 -0300

    Sliding Tabs - parte II

--- app/src/main/java/android/guilhermedambros/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/android/guilhermedambros/whatsappclone/activity/MainActivity.java
@@ -2,7 +2,11 @@ package android.guilhermedambros.whatsappclone.activity;
 
 import android.content.Intent;
 import android.guilhermedambros.whatsappclone.R;
+import android.guilhermedambros.whatsappclone.adapter.TabAdapter;
 import android.guilhermedambros.whatsappclone.config.ConfiguracaoFirebase;
+import android.guilhermedambros.whatsappclone.helper.SlidingTabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSair;
     private FirebaseAuth autenticacao;
     private Toolbar toolbar;
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
 
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.stl_tabs);
+        viewPager = (ViewPager) findViewById(R.id.vp_pagina);
+
+        //Configurar Sliding Tabs
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+
+        //Configurar Adapter
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        slidingTabLayout.setViewPager(viewPager);
 
     }
 
