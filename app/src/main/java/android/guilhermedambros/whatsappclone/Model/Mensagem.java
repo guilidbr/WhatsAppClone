b474b15534fba81c971c617231a6c1d072c2e0d0
Guilherme Dambrós <Guilherme Dambrs>
2017-04-07 20:41:57 -0300

    Salvando texto da mensagem no firebase

---
+++ app/src/main/java/android/guilhermedambros/whatsappclone/Model/Mensagem.java
@@ -0,0 +1,29 @@
+package android.guilhermedambros.whatsappclone.Model;
+
+/**
+ * Created by guili on 07/04/2017.
+ */
+
+public class Mensagem {
+    private String idUsuario;
+    private String mensagem;
+
+    public Mensagem() {
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
--- app/src/main/java/android/guilhermedambros/whatsappclone/activity/ConversaActivity.java
+++ app/src/main/java/android/guilhermedambros/whatsappclone/activity/ConversaActivity.java
@@ -1,32 +1,96 @@
 package android.guilhermedambros.whatsappclone.activity;
 
+import android.guilhermedambros.whatsappclone.Model.Mensagem;
+import android.guilhermedambros.whatsappclone.config.ConfiguracaoFirebase;
+import android.guilhermedambros.whatsappclone.helper.Base64Custom;
+import android.guilhermedambros.whatsappclone.helper.Preferencias;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.guilhermedambros.whatsappclone.R;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class ConversaActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText editMensagem;
+    private ImageButton btnMensagem;
+    private DatabaseReference firebase;
 
     //dados destinatario
     private String nomeUsuarioDestinatario;
+    private String idUsuarioDestinatario;
+
+    //dados do remetente
+    private String idUsuarioRemetente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversa);
 
         toolbar = (Toolbar) findViewById(R.id.tb_conversa);
+        editMensagem = (EditText) findViewById(R.id.edit_mensagem);
+        btnMensagem = (ImageButton) findViewById(R.id.btn_enviar);
+
+        //dados do usuario logado
+        Preferencias preferencias = new Preferencias(ConversaActivity.this);
+        idUsuarioRemetente = preferencias.getIdentificador();
+
         Bundle extra = getIntent().getExtras();
 
         if (extra != null){
             nomeUsuarioDestinatario = extra.getString("nome");
+            String emailDestinatario = extra.getString("email");
+            idUsuarioDestinatario = Base64Custom.codificarBase64(emailDestinatario);
         }
 
         //config toolbar
         toolbar.setTitle(nomeUsuarioDestinatario);
         toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
         setSupportActionBar(toolbar);
+
+        //enviar msgm
+        btnMensagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoMensagem = editMensagem.getText().toString();
+
+                if (textoMensagem.isEmpty()){
+                    Toast.makeText(ConversaActivity.this, "Digite uma mensagem para enviar!", Toast.LENGTH_LONG).show();
+                }else{//salva os dados no firebase
+
+                    Mensagem mensagem = new Mensagem();
+                    mensagem.setIdUsuario(idUsuarioRemetente);
+                    mensagem.setMensagem(textoMensagem);
+
+                    salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+                    editMensagem.setText("");
+
+                }
+            }
+        });
+
+    }
+
+    private boolean salvarMensagem(String idRemetente, String idDestinatario, Mensagem mensagem){
+        try {
+            firebase = ConfiguracaoFirebase.getFirebase();
+            firebase.child("mensagens")
+                    .child(idRemetente)
+                    .child(idDestinatario)
+                    .push()
+                    .setValue(mensagem);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
 
     }
 }
